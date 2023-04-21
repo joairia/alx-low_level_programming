6aee6fd61b924fc8bd9e0c50f67793632a723df1
joairia <joalafhal@gmail.com>
2023-04-21 15:49:08 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+  *main - main entry point
+  *Description: 'get the last degiti of a number'
+  *Return: 0
+  */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
