29dd37f7b20cd67f1f12cf9f5f9d2ec1d78b9245
joairia <joalafhal@gmail.com>
2023-04-21 16:05:09 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+  *main - main entry point
+  *Description: 'get the last degiti of a number'
+  *Return: 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
