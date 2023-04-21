c934edb3fd42df99afa24d7bd8e7ee69969dbe30
joairia <joalafhal@gmail.com>
2023-04-21 16:16:19 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+  *main - main entry point
+  *Description: 'get the last degiti of a number'
+  *Return: 0
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
