ad10db08b469c1efeec760935d1d011f84082508
joairia <joalafhal@gmail.com>
2023-04-26 08:04:25 -0700

    task0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * positive_or_negative - checks if a number is positive or negative
+ * @i: the number
+ * Return: nothing
+ */
+
 void positive_or_negative(int i)
 {
 	if (i > 0)
